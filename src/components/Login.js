3cf3c0839c89320679dde84cb19a15a36be16af3
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-11-15 02:15:48 -0500

    made-validation-schema

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,8 @@
 import React, {useState} from "react";
+import * as yup from "yup";
+import schema from "./validation/loginSchema"
+import Styled from "styled-components";
+
 
 
 const initialValues = {
@@ -6,26 +10,46 @@ const initialValues = {
     password: "",
 }
 
+const initialErrors = {
+    username: "",
+    password: "",
+}
+
 const Login  = () => {
-    const[values, setValues] = useState(initialValues);
+    const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState(initialErrors);
     
-    const onchange = (evt) => {
+    const Change = (evt) => {
         const correctValue = evt.target.value;
-        setValues({...values,[evt.target.name] : correctValue})
+        const validate = () => {
+            yup
+            .reach(schema, evt.target.name)
+            .validate(correctValue)
+            .then((res) => {
+                setErrors({...values, [evt.target.name] : ""})
+                console.log(res)
+            })
+            .catch((err) => {
+                setErrors({...errors,[evt.target.name] : err.message})
+            })
+        }
+        validate()
+        setValues({...values,[evt.target.name] : correctValue});
     }
 
 
     return(
         <form>
             <label>
-                <input 
+                <StyledInput 
                 type="text" 
                 name="username" 
                 placeholder="Username"
                 value={values.username}
-                onChange={onchange}
-                ></input>
+                onChange={Change}
+                ></StyledInput>
             </label>
+            {errors.username ? <div>{errors.username}</div> : ""}
             <br/>
             <label>
                 <input 
@@ -33,15 +57,23 @@ const Login  = () => {
                 name="password" 
                 placeholder="Password"
                 value={values.password}
-                onChange={onchange}
+                onChange={Change}
                 ></input>
             </label>
+            {errors.password ? <div>{errors.password}</div> : ""}
             <br/>
             <button>Login</button>
         </form>
-
 
     );
 };
 
 export default Login;
+
+const StyledInput = Styled.input`
+  padding:10px;
+  border:0;
+  box-shadow:0 0 15px 5px rgba(0,0,0,0.06);
+  /* margin:10px 0;  add top and bottom margin */
+  width: 100%;
+`
---
+++ src/components/validation/loginSchema.js
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+
+export default yup.object().shape({
+    username: yup
+    .string()
+    .required("Please Enter Username"),
+    password: yup
+    .string()
+    .required("Please Enter Password"),
+
+})
