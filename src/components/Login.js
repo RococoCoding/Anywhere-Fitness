2ae4134b63a784f84fbe3ad95a1a86139c4ef9e6
Alice Chang <63833718+RococoCoding@users.noreply.github.com>
2020-11-17 13:33:14 -0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import * as yup from "yup";
 import schema from "./validation/loginSchema"
 import Styled from "styled-components";
-//testing testing 2
 
 
 const initialValues = {
